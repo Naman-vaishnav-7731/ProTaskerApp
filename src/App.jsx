e959aec521d7aa3e744719da9e6219171c8db68b
= <=>
2023-09-09 12:22:46 +0530

    app setup | intial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return(
+    <h1>this is my protasker app</h1>
+  )
+}
+
+export default App;
---
+++ src/routers.jsx
@@ -0,0 +1,8 @@
+const Routers = () => {
+    return(
+        <h1>this is routers component</h1>
+    )
+}
+
+export default Routers;
+
